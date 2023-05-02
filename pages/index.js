9cb0413b1dc2d04cc85f1d42e3928910eba5be97
ihor-vip <vip20140902@gmail.com>
2023-05-02 15:46:09 +0300

    create skeleton for app( navigation between different pages)

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import {useSession} from "next-auth/react";
+import Layout from "@/components/Layout";
 
 export default function Home() {
-    const { data: session } = useSession();
-    if(!session) {
-        return (
-            <div className="bg-blue-900 w-screen h-screen flex items-center">
-                <div className="text-center w-full">
-                    <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+    const {data: session} = useSession();
+    console.log(session)
+    return (
+        <Layout>
+            <div className="text-blue-900 flex justify-between">
+                <h2 className="">
+                    Hello, <b>{session?.user?.name}</b>
+                </h2>
+                <div className="flex bg-gray-300 text-black gap-1 rounded-lg overflow-hidden">
+                    <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+                    <span className="px-2">
+                        {session?.user?.name}
+                    </span>
                 </div>
             </div>
-        );
-    }
-    return (
-        <div>Logged in {session.user.email}</div>
+        </Layout>
     )
 }
