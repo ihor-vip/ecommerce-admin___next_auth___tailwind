95b148d1d807337e58d74f539dd910bd97c2dba9
ihor-vip <vip20140902@gmail.com>
2023-05-03 13:48:44 +0300

    add product(create), add to db

---
+++ pages/api/products.js
@@ -0,0 +1,15 @@
+import {Product} from "@/models/Product";
+import {mongooseConnect} from "@/lib/mongoose";
+
+export default async function handler(req, res) {
+    const {method} = req;
+    await mongooseConnect();
+
+    if (method === "POST") {
+        const {title, description, price} = req.body;
+        const productDoc = await Product.create({
+            title, description, price
+        })
+        res.json(productDoc);
+    }
+}
--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import Layout from "@/components/Layout";
 
 export default function Products() {
     return (
         <Layout>
-            products page here
+            <Link className="bg-blue-900 text-white rounded-md py-1 px-2" href={"/products/new"}>Add new product</Link>
         </Layout>
     )
 }
