23e835388845d7a2ea90add55b9c0b81766e0695
ihor-vip <vip20140902@gmail.com>
2023-05-05 12:14:19 +0300

    add new property to product Category page

--- pages/categories.js
+++ pages/categories.js
@@ -7,6 +7,7 @@ function Categories({swal}) {
     const [name, setName] = useState('');
     const [parentCategory, setParentCategory] = useState('');
     const [categories, setCategories] = useState([]);
+    const [properties, setProperties] = useState([]);
     const [editedCategory, setEditedCategory] = useState(null);
 
     useEffect(() => {
@@ -55,6 +56,28 @@ function Categories({swal}) {
             }
         });
     }
+
+    function addProperty() {
+        setProperties(prev => {
+            return [...prev, {name: '', values: ''}]
+        })
+    }
+
+    function handlePropertyNameChange(index, property, newName) {
+        setProperties(prev => {
+            const properties = [...prev];
+            properties[index].name = newName;
+            return properties;
+         })
+    }
+
+    function handlePropertyValuesChange(index, property, newValues) {
+        setProperties(prev => {
+            const properties = [...prev];
+            properties[index].values = newValues;
+            return properties;
+        })
+    }
     return (
         <div>
             <Layout>
@@ -65,21 +88,45 @@ function Categories({swal}) {
                         : 'Create new category'
                     }
                 </label>
-                <form onSubmit={saveCategory} className="flex gap-1">
-                    <input
-                        className='mb-0'
-                        type="text"
-                        placeholder={'Category name'}
-                        value={name}
-                        onChange={ev => setName(ev.target.value)}
-                    />
+                <form onSubmit={saveCategory}>
+                    <div className="flex gap-1">
+                        <input
+                            type="text"
+                            placeholder={'Category name'}
+                            value={name}
+                            onChange={ev => setName(ev.target.value)}
+                        />
 
-                    <select className="mb-0" onChange={ev => setParentCategory(ev.target.value)} value={parentCategory}>
-                        <option value="">No parent category</option>
-                        {categories.length > 0 && categories.map(category => (
-                            <option value={category._id}>{category.name}</option>
+                        <select onChange={ev => setParentCategory(ev.target.value)} value={parentCategory}>
+                            <option value="">No parent category</option>
+                            {categories.length > 0 && categories.map(category => (
+                                <option value={category._id}>{category.name}</option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="mb-2">
+                        <label className="block">Properties</label>
+                        <button type="button" onClick={addProperty} className="btn-default text-sm mb-2">Add new property</button>
+
+                        {properties.length > 0 && properties.map((property, index) => (
+                            <div className="flex gap-1">
+                                <input
+                                    type="text"
+                                    value={property.name}
+                                    onChange={(ev) => handlePropertyNameChange(index, property, ev.target.value)}
+                                    placeholder="property name (example: color)"
+                                />
+
+                                <input
+                                    type="text"
+                                    value={property.values}
+                                    onChange={(ev) => handlePropertyValuesChange(index, property, ev.target.value)}
+                                    placeholder="values"
+                                />
+                            </div>
                         ))}
-                    </select>
+                    </div>
 
                     <button type='submit' className="btn-primary py-1">Save</button>
                 </form>
